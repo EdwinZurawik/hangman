1817d2b51e279f200e4ddea99e86a7b24dc63014
Edwin Żurawik <ezurawik35@gmail.com>
2021-06-05 10:43:35 +0200

    added a select word functionality

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,21 +1,30 @@
 import random
 import hangman_art as ha
 
-words = [ 'first', 'second', 'four' ]
+words = ['first', 'second', 'four']
+
+class Hangman():
+
+    def __init__(self):
+        pass
+
+    def draw_menu(self):
+        print(ha.hangman_title, ha.hangman_stages[-1])
+
+    def select_word(self, words):
+        selected_word = random.choice(words)
+        return selected_word
 
 def main():
     end_game = False
+    hangman = Hangman()
 
     while not end_game:
-        draw_menu()
+        hangman.draw_menu()
+        word = hangman.select_word(words)
+        print(f"Your selected word is: {word}")
         input("Press any key to exit.")
         end_game = True
-
-def draw_menu():
-    print(ha.hangman_title, ha.hangman_stages[-1])
-
-def select_word(words):
-    selected_word = random.choice(words)
 
 if __name__ == '__main__':
     main()
