908aa1d4bb8ae2944472271448612887c52eb282
Edwin Żurawik <ezurawik35@gmail.com>
2021-06-05 15:30:41 +0200

    feat: finished game mechanic
    -added tests for guess_a_letter method

--- src/hangman.py
+++ src/hangman.py
@@ -6,10 +6,23 @@ words = ['first', 'second', 'four']
 class Hangman():
 
     def __init__(self):
-        pass
+        self.selected_word = ""
+        self.guessed_letters = []
+        self.misses = 0
 
-    def draw_menu(self):
-        print(ha.hangman_title, ha.hangman_stages[-1])
+    def guess_a_letter(self, letter):
+        if not isinstance(letter, str):
+            raise ValueError(f"Expected a string but got {type(letter)} instead.")
+        if len(letter) != 1:
+            raise ValueError(f"Expected a single character string but got {len(letter)} characters instead.")
+        if not letter.isalpha():
+            raise ValueError(f"Expected a letter but got {letter} instead.")
+        letter = letter.lower()
+        for index, l in enumerate(self.selected_word):
+            if letter == l:
+                self.guessed_letters[index] = letter
+        if letter not in self.guessed_letters:
+            self.misses += 1
 
     def select_word(self, words):
         if not isinstance(words, list):
@@ -18,19 +31,55 @@ class Hangman():
             raise ValueError("Words list should not be empty.")
         if not all(isinstance(word, str) for word in words):
             raise ValueError(f"Expected a list of type {str}. It should not contain elements of other types.")
-        selected_word = random.choice(words)
-        return selected_word
+        self.selected_word = random.choice(words)
+        self.guessed_letters = ["_" for letter in self.selected_word]
+
+    def check_if_game_ended(self):
+        if (self.misses >= len(ha.hangman_stages)-1) or list(self.selected_word) == self.guessed_letters:
+            return True
+        return False
+
+
+def draw_menu():
+    print(ha.hangman_title, ha.hangman_stages[-1])
+    print("1. Start a new game.\n2. Exit.\n")
+
+
+def draw_game_table(hangman):
+    print(" ".join(hangman.guessed_letters))
+    print(ha.hangman_stages[hangman.misses])
+
+def print_end_game_communicate(hangman):
+    if "_" not in hangman.guessed_letters:
+        print(f"Congratulations! You have guessed a word: \"{hangman.selected_word}\".")
+    else:
+        print(f"You have lost. But don't worry, you can try one more time!")
+
 
 def main():
     end_game = False
-    hangman = Hangman()
+    exit_program = False
 
-    while not end_game:
-        hangman.draw_menu()
-        word = hangman.select_word(words)
-        print(f"Your selected word is: {word}")
-        input("Press any key to exit.")
-        end_game = True
+    while not exit_program:
+        draw_menu()
+        choice = input("Your choice: ")
+
+        if choice == "1":
+            end_game = False
+            hangman = Hangman()
+            hangman.select_word(words)
+            draw_game_table(hangman)
+            while not end_game:
+                try:
+                    hangman.guess_a_letter(input("Select a letter: "))
+                except ValueError:
+                    print("You have entered an incorrect value. Try again.")
+                draw_game_table(hangman)
+                end_game = hangman.check_if_game_ended()
+            print_end_game_communicate(hangman)
+            input("Press any key to proceed..")
+        elif choice == "2":
+            exit_program = True
 
 if __name__ == '__main__':
     main()
--- tests/hangman_test.py
+++ tests/hangman_test.py
@@ -4,18 +4,27 @@ import pytest
 from src.hangman import Hangman
 
 
+# test Hangman.select_word(self, words)
 def test_select_word_from_one_element_list() -> None:
     hangman = Hangman()
     words_list = ["word"]
 
-    assert hangman.select_word(words_list) == words_list[0]
+    hangman.select_word(words_list)
+    
+    assert hangman.selected_word == words_list[0]
+    assert len(hangman.guessed_letters) == len(hangman.selected_word)
+    assert all([letter == "_" for letter in hangman.guessed_letters])
 
 
 def test_select_word_from_multiple_elements_list() -> None:
     hangman = Hangman()
-    words_list = ["word1", "word2", "word3"]
+    words_list = ["first", "second", "third"]
 
-    assert hangman.select_word(words_list) in words_list
+    hangman.select_word(words_list)
+
+    assert hangman.selected_word in words_list
+    assert len(hangman.guessed_letters) == len(hangman.selected_word)
+    assert all([letter == "_" for letter in hangman.guessed_letters])
 
 
 def test_select_word_from_an_empty_list() -> None:
@@ -40,3 +49,65 @@ def test_select_word_list_contains_not_strings() -> None:
 
     with pytest.raises(ValueError):
         hangman.select_word(words_list)
+
+
+# test Hangman.guess_a_letter(self, letter)
+def test_guess_a_letter_correct_letter_guessed() -> None:
+    hangman = Hangman()
+    hangman.select_word(["word"])
+    hangman.guessed_letters = ["_", "_", "_", "_"]
+    letter = "w"
+
+    hangman.guess_a_letter(letter)
+    assert hangman.misses == 0
+    assert hangman.selected_word == "word"
+    assert hangman.guessed_letters == ["w", "_", "_", "_"]
+
+
+def test_guess_a_letter_uppercase_letter_guessed() -> None:
+    hangman = Hangman()
+    hangman.select_word(["word"])
+    letter = "D"
+
+    hangman.guess_a_letter(letter)
+    assert hangman.misses == 0
+    assert hangman.selected_word == "word"
+    assert hangman.guessed_letters == ["_", "_", "_", "d"]
+
+
+def test_guess_a_letter_incorrect_letter_guessed() -> None:
+    hangman = Hangman()
+    hangman.select_word(["word"])
+    letter = "z"
+
+    hangman.guess_a_letter(letter)
+    assert hangman.misses == 1
+    assert hangman.selected_word == "word"
+    assert hangman.guessed_letters == ["_", "_", "_", "_"]
+
+
+def test_guess_a_letter_incorrect_argument_type() -> None:
+    hangman = Hangman()
+
+    with pytest.raises(ValueError):
+        hangman.guess_a_letter(1)
+
+
+def test_guess_a_letter_empty_string() -> None:
+    hangman = Hangman()
+
+    with pytest.raises(ValueError):
+        hangman.guess_a_letter("")
+
+
+def test_guess_a_letter_too_long_string() -> None:
+    hangman = Hangman()
+
+    with pytest.raises(ValueError):
+        hangman.guess_a_letter("word")
+
+def test_guess_a_letter_not_letter() -> None:
+    hangman = Hangman()
+
+    with pytest.raises(ValueError):
+        hangman.guess_a_letter("1")
